4a1f164b0efc3887acc8ba6a6c40904def050f0f
B3TA-BLOCKER <hassaanalibukhari@gmail.com>
2024-07-30 12:26:25 +0500

    check if nmap is installed

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import os 
+import subprocess
+import platform
+
+
+result = subprocess.run(['nmap', '--version'])
+
+def install_nmap():
+    os_type = platform.system()
+    try:
+        if os_type == 'Linux' :
+            distro = platform.linux_distribution()[0].lower()
+            if 'ubuntu' in distro or 'debian' in distro :
+                subprocess.run(['sudo', 'apt-get' , 'update'], check =True)
+                subprocess.run(['sudo' , 'apt-get' , 'install' , '-y' , 'nmap'],check=True)
+            elif 'fedora' in distro or 'centos' in distro or 'rhel' in distro:
+                subprocess.run(['sudo', 'dnf', 'install' , '-y' , 'nmap'],check=True)
 
 
 def recon(ip):
@@ -6,4 +22,8 @@ def recon(ip):
     os.system(f"dirb {ip}")
 
 
-recon(input("Enter the ip: "))
+if result.returncode == 0: 
+    recon(input("Enter the ip: "))
+else:
+    install_nmap()
+
